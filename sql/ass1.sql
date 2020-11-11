7fc14a1d89a185f07d7f1ce8dd7921c65225e4bf
Natchariya Wongamnuayporn <65109744+Natchariyawong25@users.noreply.github.com>
2020-11-11 12:33:04 +0700

    Update ass1.sql

--- sql/ass1.sql
+++ sql/ass1.sql
@@ -5,17 +5,17 @@ FROM "EMPLOYEE" e
 JOIN "DEPARTMENT" d ON d."Dnumber" = e."Dno"
 GROUP BY d."Dname"
 HAVING AVG("Salary") > 30000 ;
-
+/* can query */ 
 
 -- ข้อ b
 SELECT	e."Sex" as "Gender",
-		d."Dname" as "Department_name" ,
-		count(e."Dno") as "Number_Male_Employee"
+	d."Dname" as "Department_name" ,
+	count(e."Dno") as "Number_Male_Employee"
 FROM "EMPLOYEE" e
 JOIN "DEPARTMENT" d ON d."Dnumber" = e."Dno"
 WHERE e."Sex" = 'M'AND e."Dno" IN (SELECT e."Dno" FROM "EMPLOYEE" e
-								   GROUP BY e."Dno" 
-								   HAVING AVG("Salary") > 30000 )
+				   GROUP BY e."Dno" 
+				   HAVING AVG("Salary") > 30000 )
 GROUP BY d."Dname" , e."Sex" ; 
 /* can query */ 
 
